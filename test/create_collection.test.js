91ee429300c7e2bfc061a71ce91be9455d984592
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-11-10 16:02:22 +0100

    write test for create collection

---
+++ test/create_collection.test.js
@@ -0,0 +1,71 @@
+/* eslint no-console: 0 */
+/* eslint prefer-arrow-callback: 0 */
+
+import { expect, assert } from 'chai';
+import {run, generateMeta} from 'syncano-test';
+
+import dotenv from 'dotenv';
+
+import Rekognition from '../src/utils/Rekognition';
+
+dotenv.config({ silent: process.env.NODE_ENV === 'production' });
+
+const obj = { a: 24 };
+
+describe('check obj', () => {
+  it('obj should not be empty', () => {
+    expect(Object.keys(obj).length > 0).to.equal(true);
+    assert.propertyVal(obj, 'a', 24);
+  });
+});
+
+describe('create_collection', () => {
+  const meta = generateMeta();
+
+  const config = {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+  };
+
+  const args = {
+    collectionId: 'collectionTest'
+  };
+
+  after((done) => {
+    const awsRekognitionClass = new Rekognition(config);
+    awsRekognitionClass.deleteCollection(args.collectionId)
+      .then(() => {
+        console.log('Cleared');
+        done();
+      });
+  });
+
+  it('with valid collection name', (done) => {
+    run('create_collection', {args, meta, config})
+      .then((res) => {
+        assert.propertyVal(res, 'code', 200);
+        assert.propertyVal(res, 'mimetype', 'application/json');
+        assert.propertyVal(res.data, 'statusCode', 200);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('without collection name', (done) => {
+    const argsWithoutData = {};
+
+    run('create_collection', {argsWithoutData, meta, config})
+      .then((res) => {
+        assert.propertyVal(res, 'code', 400);
+        assert.propertyVal(res, 'mimetype', 'application/json');
+        assert.propertyVal(res.data, 'message', 'Missing required key \'CollectionId\' in params');
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
