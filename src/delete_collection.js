853f5c898eb02ed656c5a378e13d44e41933664e
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-11-13 13:29:45 +0100

    refactor endpoints

--- src/delete_collection.js
+++ src/delete_collection.js
@@ -5,13 +5,7 @@ import Rekognition from './utils/Rekognition';
 export default (ctx) => {
   const {response} = Syncano(ctx);
 
-  const config = {
-    AWS_ACCESS_KEY_ID: ctx.config.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: ctx.config.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: ctx.config.AWS_REGION,
-  };
-
-  const awsRekognitionClass = new Rekognition(config);
+  const awsRekognitionClass = new Rekognition(ctx.config);
 
   return awsRekognitionClass.deleteCollection(ctx.args.collectionId)
     .then((res) => {
